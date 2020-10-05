1fb3fb72dd3e651d6044b93aa5e34638225bb201
Slim Hmidi <slim.hmidi1@gmail.com>
2020-10-05 23:55:03 +0200

    WIP: add debounce function

--- frontend/src/app/utils.ts
+++ frontend/src/app/utils.ts
@@ -8,3 +8,17 @@ export const isAuthenticated = () => {
 
 export const formatDate = (date: Date, dateFormat: string) =>
   format(date, dateFormat);
+
+export const debounce = <F extends ((...args: any[]) => any)>(
+  func: F,
+  waitFor: number,
+) => {
+  let timeout: number = 0;
+
+  const debounced = (...args: any[]) => {
+    clearTimeout(timeout);
+    setTimeout(() => func(...args), waitFor);
+  };
+
+  return debounced as (...args: Parameters<F>[]) => ReturnType<F>;
+};
